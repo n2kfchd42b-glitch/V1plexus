ae741873f7de7151b9ad9b68af13a78b17495a34
Seth Kofi Abrokwa <sethkofiabrokwa@MacBookAir.fritz.box>
2026-03-22 02:19:29 +0100

    Remove eslint key from next.config.ts (not supported in Next.js 16)

--- next.config.ts
+++ next.config.ts
@@ -4,11 +4,6 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [],
   },
-  eslint: {
-    // Pre-existing unused-import warnings in analysis/lib files don't affect runtime.
-    // Run `npm run lint` locally to review; don't block production deploys.
-    ignoreDuringBuilds: true,
-  },
 };
 
 export default nextConfig;
